c4f43833d06194563e973a6f1e396bb527ab1f7c
helben2002 <helben2002@goteborg.skola.se>
2024-04-16 14:15:04 +0200

    fetch on subject function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-let test = document.getElementById('test')
-test.innerText = "Hello"
+async function getBooksBySubject(subject) {
+    const LIMIT = 100
+    let resp = await fetch(`https://openlibrary.org/subjects/${subject}.json?limit=${LIMIT}`)
+    let json = await resp.json()
+
+    let bookList = []
+    bookList.push(json.works)
+
+    let bookContainer = document.getElementById('books')
+    for (let i = 0; i < 10; i++) {
+        let bookDiv = document.createElement('div')
+        let randBook = (bookList[0][(Math.floor(Math.random()*100))])
+
+        console.log(randBook)
+
+        bookDiv.textContent = randBook.title + " - " + randBook.authors[0].name
+        bookContainer.appendChild(bookDiv)
+    }
+}
+
+getBooksBySubject('fiction')
