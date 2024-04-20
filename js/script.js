6dbfe66051868972a2942b575e8370763b8f627f
Älva <alvlac0326@skola.goteborg.se>
2024-04-20 15:10:02 +0200

    fixed duplication in subject search and created a search book with inner text function

--- js/script.js
+++ js/script.js
@@ -4,18 +4,35 @@ async function getBooksBySubject(subject) {
     let json = await resp.json()
 
     let bookList = []
-    bookList.push(json.works)
+    bookList.push(json.works);
 
     let bookContainer = document.getElementById('books')
+
+    let randNumChecker = []
+
     for (let i = 0; i < 10; i++) {
+        let randomNum = Math.floor(Math.random() * LIMIT);
+
+        //Check if book has already been pushed
+        if (randNumChecker.includes(randomNum)) {
+            //Bring back looper and restart current loop
+            i--;
+            continue
+        } else {
+            randNumChecker.push(randomNum)
+        }
+
         let bookDiv = document.createElement('div')
-        let randBook = (bookList[0][(Math.floor(Math.random()*100))])
+        let randBook = (bookList[0][randomNum])
 
-        console.log(randBook)
 
-        bookDiv.textContent = randBook.title + " - " + randBook.authors[0].name
+        console.log(i + " " + randBook)
+
+        bookDiv.textContent = i + ". " + randBook.title + " - " + randBook.authors[0].name
         bookContainer.appendChild(bookDiv)
     }
+
+    randNumChecker = []
 }
 
 getBooksBySubject('fiction')
