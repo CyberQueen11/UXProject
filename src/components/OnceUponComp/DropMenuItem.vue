8a5ceed657de3503872262a69d2ec522cd5630b8
Älva <alvlac0326@skola.goteborg.se>
2024-05-10 09:05:44 +0200

    files added to be commited

---
+++ src/components/OnceUponComp/DropMenuItem.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="item">
+      <h3>
+        <slot name="text"></slot>
+      </h3>
+    </div>
+  </template>
+  
---
+++ src/components/OnceUponComp/OnceUponComp.vue
@@ -0,0 +1,28 @@
+<script setup>
+import DropMenuItem from './DropMenuItem.vue'
+</script>
+
+<template>
+  <DropMenuItem>
+    <template #text>Once upon a time in the era of 
+      <select class="select w-full max-w-xs">
+  <option disabled selected>Pick your favorite Simpson</option>
+  <option>Homer</option>
+  <option>Marge</option>
+  <option>Bart</option>
+  <option>Lisa</option>
+  <option>Maggie</option>
+</select>
+     </template>
+  </DropMenuItem>
+
+  <DropMenuItem>
+    <template #text>There was a ---</template>
+  </DropMenuItem>
+
+  <DropMenuItem>
+    <template #text>who lived in ---</template>
+  </DropMenuItem>
+
+  <button>Reveal the book</button>
+</template>
