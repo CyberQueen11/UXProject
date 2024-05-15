c3b43efc267b4dc3776ca8baf0ff9f1a1ea85175
helben2002 <helben2002@skola.goteborg.se>
2024-05-15 17:31:09 +0200

    pictures and some style updated

--- src/components/pictureSwipe/PicGenre.vue
+++ src/components/pictureSwipe/PicGenre.vue
@@ -1,40 +1,50 @@
 <template>
+  <div class="">
     <h2 class="mx-2">{{ title }}</h2>
     <div class="carousel">
       <div class="carousel-item my-4 mx-1">
         <ClickPic subject="horror" />
       </div>
-  
+
       <div class="carousel-item my-4 mx-1">
         <ClickPic subject="love" />
       </div>
-  
+
       <div class="carousel-item my-4 mx-1">
         <ClickPic subject="fiction" />
       </div>
-  
+
       <div class="carousel-item my-4 mx-1">
         <ClickPic subject="fiction" />
       </div>
-  
+
       <div class="carousel-item my-4 mx-1">
         <ClickPic subject="fiction" />
+      </div>
+
+      <div class="carousel-item my-4 mx-1">
+        <ClickPic subject="love" />
+      </div>
+
+      <div class="carousel-item my-4 mx-1">
+        <ClickPic subject="love" />
       </div>
     </div>
-  </template>
-  
-  <script>
-  import ClickPic from './ClickPic.vue'
-  
-  export default {
-    name: 'PicContain',
-    components: { ClickPic },
-    data() {
-      return {
-        title: 'Genre, Topic, Location, Time',
-        genre: ''
-      }
-    },
-    methods: {}
-  }
-  </script>
+  </div>
+</template>
+
+<script>
+import ClickPic from "./ClickPic.vue";
+
+export default {
+  name: "PicContain",
+  components: { ClickPic },
+  data() {
+    return {
+      title: "Genre, Topic, Location, Time",
+      genre: "",
+    };
+  },
+  methods: {},
+};
+</script>
--- src/views/PhotoSwipe.vue
+++ src/views/PhotoSwipe.vue
@@ -1,21 +1,36 @@
 <template>
-  <h1 class="m-8 text-center font-bold">{{ title }}</h1>
-  <div class="m-4">
-    <PicGenre />
-    <PicGenre />
-    <PicGenre />
-    <PicGenre />
-    <GenerateButton />
+  <div class="flex flex-col items-center">
+    <HomeButton class="m-4" />
+
+    <div class="relative flex justify-center mt-20 lg:mt-32">
+      <h2 class="font-quattrocento custom-text-red text-2xl lg:text-4xl">
+        Picture swipe
+      </h2>
+    </div>
+
+    <img class="h-48 w-64" src="../assets/icons/Pic_swipe.png" alt="" />
+
+    <h3>{{ title }}</h3>
+    <div class="m-4">
+      <PicGenre />
+      <PicGenre />
+      <PicGenre />
+      <PicGenre />
+      <GenerateButton />
+    </div>
+    <AboutButton />
   </div>
 </template>
 
 <script>
 import PicGenre from "../components/pictureSwipe/PicGenre.vue";
 import GenerateButton from "../components/pictureSwipe/GenerateButton.vue";
+import HomeButton from "@/components/general_components/HomeButton.vue";
+import AboutButton from "@/components/general_components/AboutButton.vue";
 
 export default {
   name: "App",
-  components: { PicGenre, GenerateButton },
+  components: { PicGenre, GenerateButton, HomeButton, AboutButton },
   data() {
     return {
       title: "Choose one image in each category",
