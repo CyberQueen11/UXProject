a4bbfb6fe14c862026029f5069b5109631ed0d6b
Poya Kazemzade <pouya.pua@gmail.com>
2024-05-15 20:24:42 +0200

    created header for judge

--- src/views/JudgeTheBookByItsCover.vue
+++ src/views/JudgeTheBookByItsCover.vue
@@ -1,4 +1,5 @@
 <script setup>
+import AboutButton from "@/components/general_components/AboutButton.vue";
 import HomeButton from "../components/general_components/HomeButton.vue";
 import BookFetcher from "../components/judgeTheBookByCover/BookFetcher.vue";
 </script>
@@ -6,8 +7,29 @@ import BookFetcher from "../components/judgeTheBookByCover/BookFetcher.vue";
 <template>
   <header>
     <HomeButton />
+    <!-- mobile -->
+    <div class="block lg:hidden">
+      <div class="flex flex-col items-center text-center mt-24 mb-10">
+        <h2 class="font-Quattrocento text-darkred text-[31px] mb-6">
+          Judge the <br />book by its <br />cover
+        </h2>
+        <img src="../assets/icons/Streck.png" alt="separator" />
+      </div>
+      <h4 class="font-Quattrocento text-darkred text-[20px] hidden">
+        Judge the <br />book by its <br />cover
+      </h4>
+    </div>
+    <!-- desktop -->
+    <div class="hidden lg:block flex flex-col items-center text-center mt-44">
+      <h2 class="font-Quattrocento text-darkred text-[31px] mb-6">
+        Judge the book by its cover
+      </h2>
+    </div>
   </header>
   <main>
     <BookFetcher />
   </main>
+  <footer class="hidden lg:block">
+    <AboutButton />
+  </footer>
 </template>
