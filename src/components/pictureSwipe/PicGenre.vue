6fc218dbcec338d6800eca932b9b1c55781a962b
Älva <alvlac0326@skola.goteborg.se>
2024-05-16 16:37:18 +0200

    changed paths

--- src/components/pictureSwipe/PicGenre.vue
+++ src/components/pictureSwipe/PicGenre.vue
@@ -3,27 +3,27 @@
     <h2 class="mx-2">{{ title }}</h2>
     <div class="carousel">
       <div class="carousel-item my-4 mx-1">
-        <img class="" src="..\assets\Picture_swipe\Genre\Fantasy.png" alt="pic" @click="$emit('myEvent', 'fantasy')">
+        <img class="" src="..\..\assets\Picture_swipe\Genre\Fantasy.png" alt="pic" @click="$emit('myEvent', 'fantasy')">
       </div>
 
       <div class="carousel-item my-4 mx-1">
-        <img class="" src="..\assets\Picture_swipe\Genre\Horror.png" alt="pic" @click="$emit('myEvent', 'horror')">
+        <img class="" src="..\..\assets\Picture_swipe\Genre\Horror.png" alt="pic" @click="$emit('myEvent', 'horror')">
       </div>
 
       <div class="carousel-item my-4 mx-1">
-        <img class="" src="..\assets\Picture_swipe\Genre\Mystery.png" alt="pic" @click="$emit('myEvent', 'mystery')">
+        <img class="" src="..\..\assets\Picture_swipe\Genre\Mystery.png" alt="pic" @click="$emit('myEvent', 'mystery')">
       </div>
 
       <div class="carousel-item my-4 mx-1">
-        <img class="" src="..\assets\Picture_swipe\Genre\Romance.png" alt="pic" @click="$emit('myEvent', 'romance')">
+        <img class="" src="..\..\assets\Picture_swipe\Genre\Romance.png" alt="pic" @click="$emit('myEvent', 'romance')">
       </div>
 
       <div class="carousel-item my-4 mx-1">
-        <img class="" src="..\assets\Picture_swipe\Genre\Science fiction.png" alt="pic" @click="$emit('myEvent', 'fiction')">
+        <img class="" src="..\..\assets\Picture_swipe\Genre\Science fiction.png" alt="pic" @click="$emit('myEvent', 'fiction')">
       </div>
 
       <div class="carousel-item my-4 mx-1">
-        <img class="" src="..\assets\Picture_swipe\Genre\Thriller.png" alt="pic" @click="$emit('myEvent', 'thriller')">
+        <img class="" src="..\..\assets\Picture_swipe\Genre\Thriller.png" alt="pic" @click="$emit('myEvent', 'thriller')">
       </div>
     </div>
   </div>
