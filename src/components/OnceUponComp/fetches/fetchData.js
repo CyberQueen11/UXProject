a43454a3ef253e0e747b557cff2d7ee978372a1d
Älva <alvlac0326@skola.goteborg.se>
2024-05-19 15:10:39 +0200

    Added a description output to book as well

--- src/components/OnceUponComp/fetches/fetchData.js
+++ src/components/OnceUponComp/fetches/fetchData.js
@@ -5,7 +5,7 @@ export async function fetchData(
   setBook,
   setFetching
 ) {
-  const openlibraryUrl = "https://openlibrary.org/search.json?";
+  const openlibrarySearchUrl = "https://openlibrary.org/search.json?";
   const LIMIT = 500;
   let offset = 0;
   let numFound = 0;
@@ -17,7 +17,7 @@ export async function fetchData(
   do {
     console.log(`Fetching data from Open Library with offset: ${offset}`);
 
-    let url = `${openlibraryUrl}language=eng&limit=${LIMIT}&offset=${offset}`;
+    let url = `${openlibrarySearchUrl}language=eng&limit=${LIMIT}&offset=${offset}`;
     if (selectedEra) url += `&time=${selectedEra}`;
     if (selectedCharacter) url += `&subject=${selectedCharacter}`;
     if (selectedPlace) url += `&place=${selectedPlace}`;
@@ -36,6 +36,13 @@ export async function fetchData(
           const randomBook = json.docs[randomIndex];
 
           if (randomBook.cover_i) {
+            const descripKey = randomBook.key; // for example: "/works/OL15151680W"
+            const descripURL = `https://openlibrary.org${descripKey}.json`;
+            let descripResp = await fetch(descripURL);
+            let descripJSON = await descripResp.json();
+
+            let bookDescription = extractDescription(descripJSON);
+
             const newBook = {
               title: randomBook.title,
               author: randomBook.author_name
@@ -44,6 +51,13 @@ export async function fetchData(
               isbn: randomBook.isbn ? randomBook.isbn[0] : "Unknown",
               publish_year: randomBook.publish_year
                 ? randomBook.publish_year[0]
+                : "Unknown",
+              summary: bookDescription || "No summary available",
+              language: randomBook.language
+                ? randomBook.language[0]
+                : "Unknown",
+              pages: randomBook.number_of_pages_median
+                ? randomBook.number_of_pages_median
                 : "Unknown",
             };
 
@@ -67,4 +81,21 @@ export async function fetchData(
       return;
     }
   } while (offset < maxSearchNum);
+}
+
+function extractDescription(descripJSON) {
+  if (descripJSON.description) {
+    if (typeof descripJSON.description === "string") {
+      return descripJSON.description;
+    } else if (
+      typeof descripJSON.description === "object" &&
+      descripJSON.description.value
+    ) {
+      return descripJSON.description.value;
+    } else {
+      return "No description";
+    }
+  } else {
+    return "No description";
+  }
 }
